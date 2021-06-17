a2c80ad7a9337dbd784f2bf1c34e9b86399c971d
Nolen Johnson <johnsonnolen@gmail.com>
2021-06-17 00:43:04 -0400

    odroidc4: Initial Commit
    Change-Id: I5bbc92935d81897929edf3daefb27a9e09691f60

---
+++ BoardConfig.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+include device/amlogic/g12-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/hardkernel/odroidc4
+
+## Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := odroidc4
---
+++ device.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/hardkernel/odroidc4
+
+$(call inherit-product, device/amlogic/g12-common/g12.mk)
