931297bd185758d3bbefd79db2bb7721f2679ecf
Nolen Johnson <johnsonnolen@gmail.com>
2022-06-08 22:41:02 -0400

    odroidc4: Inherit common tree after device vars are set
    * Allows several variables to be read in common tree.
    
    Change-Id: Ic8f3a4d210568254d814b013b67d8f06efff5e63

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,8 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-include device/amlogic/g12-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/hardkernel/odroidc4
 
@@ -16,3 +14,6 @@ TARGET_DTB_NAME := sm1_s905y3_odroidc4
 
 ## Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+## Include the common tree BoardConfig makefile
+include device/amlogic/g12-common/BoardConfigCommon.mk
--- device.mk
+++ device.mk
@@ -1,11 +1,12 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-$(call inherit-product, device/amlogic/g12-common/g12.mk)
-
 ## Factory
 PRODUCT_HOST_PACKAGES += \
     aml_image_packer
+
+## Inherit from the common tree product makefile
+$(call inherit-product, device/amlogic/g12-common/g12.mk)
