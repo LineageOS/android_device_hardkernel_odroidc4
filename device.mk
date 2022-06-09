1eb67ec4abe028f03bf7e24f5e7ac3daa35b1a21
Nolen Johnson <johnsonnolen@gmail.com>
2022-06-08 22:41:02 -0400

    odroidc4: Inherit common tree after device vars are set
    * Allows several variables to be read in common tree.
    
    Change-Id: Ic8f3a4d210568254d814b013b67d8f06efff5e63

--- device.mk
+++ device.mk
@@ -6,8 +6,34 @@
 
 DEVICE_PATH := device/hardkernel/odroidc4
 
+<<<<<<< HEAD
 $(call inherit-product, device/amlogic/g12-common/g12.mk)
+=======
+## Bluetooth
+BOARD_HAVE_BLUETOOTH_RTK_TV := true
+include hardware/realtek/rtkbt/rtkbt.mk
+>>>>>>> 6628264 (m5: Inherit common tree after device vars are set)
 
 ## Factory
 PRODUCT_HOST_PACKAGES += \
     aml_image_packer
+<<<<<<< HEAD
+=======
+
+## Init-Files
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc
+
+## Kernel Modules
+PRODUCT_PACKAGES += \
+    8822cs \
+    rtk_btusb
+
+## Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
+
+## Common Tree
+$(call inherit-product, device/amlogic/g12-common/g12.mk)
+
+>>>>>>> 6628264 (m5: Inherit common tree after device vars are set)
