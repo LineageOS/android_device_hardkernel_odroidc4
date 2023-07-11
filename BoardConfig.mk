73fae49c41e707c76bdd15597e5f3b3a401fe797
Nolen Johnson <johnsonnolen@gmail.com>
2023-07-11 16:57:03 -0400

    odroidc4: Use full super image size
    Change-Id: I78293436e7db69d356acbe7c14a22cb49e12b905
    (cherry picked from commit bd26d69f7ea1db44b5e2ae5a3b0d5b3d31d89334)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2021-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,6 +11,9 @@ TARGET_BOOTLOADER_BOARD_NAME := odroidc4
 
 ## DTB
 TARGET_DTB_NAME := sm1_s905y3_odroidc4
+
+## Partitions
+BOARD_SUPER_PARTITION_SIZE := 2084569088
 
 ## Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
