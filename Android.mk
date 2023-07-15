ad8339e00c16e0aad538f3f8df679e23beaadbf4
Nolen Johnson <johnsonnolen@gmail.com>
2023-07-14 23:09:43 -0400

    odroidc4: Allow radxa0_tab to build aml_*_image
    Change-Id: I273e2b127b007bc809af833e027a518c3235f754

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-ifeq ($(TARGET_DEVICE), odroidc4)
+ifneq ($(filter odroidc4 odroidc4_tab, $(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
