1d118858d02a97e0831a0aa8bca683b9ee4f576b
Bruno Martins <bgcngm@gmail.com>
2023-10-25 15:05:05 +0100

    odroidc4: Move TARGET_HAS_TEE flag to the proper place
    Change-Id: Idd4380e022aa6498a740fe8909a3ce02cc9e6a39

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2021-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,8 +11,11 @@ BOARD_HAVE_BLUETOOTH := false
 PRODUCT_HOST_PACKAGES += \
     aml_image_packer
 
-## Inherit from the common tree product makefile
-$(call inherit-product, device/amlogic/g12-common/g12.mk)
+## TEE
+TARGET_HAS_TEE := false
 
 ## Wi-Fi
 BOARD_HAVE_WIFI := false
+
+## Inherit from the common tree product makefile
+$(call inherit-product, device/amlogic/g12-common/g12.mk)
