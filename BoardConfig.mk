95c8b2f0d3b2711ca00116a46dbcd142b0003085
Nolen Johnson <johnsonnolen@gmail.com>
2024-01-16 23:07:36 -0500

    odroidc4: Disable the GMS SetupWraith pairing wizard
    * Users can still send the keye vnet for, or press the
      upgrade key (treated as pairing button) on the deivce
      to open BT pairing - resolves all the complaints about
      not being able to use USB keyboards, etc because SetupWraith
      prevents moving past the pairing wizard screen without
      successfully pairing a remote.
    
    Change-Id: I23d5e455d52899aec069fa0897b684433566350c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,6 +16,7 @@ TARGET_DTB_NAME := sm1_s905y3_odroidc4
 BOARD_SUPER_PARTITION_SIZE := 2084569088
 
 ## Properties
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 ## Include the common tree BoardConfig makefile
