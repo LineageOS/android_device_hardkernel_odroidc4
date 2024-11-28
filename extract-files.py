995169096e6a660617b208b4a4d36e2d267aa92f
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-28 02:18:53 -0500

    odroidc4: Drop extract setup
    * We used to use this for more, but now there are 0 files.
    * Just a bootloader image that we build out of tree, so
      extraction makes no sense. Just hand create firmware.mk @
      `vendor/amlogic/${DEVICE}/firmware.mk`, store bootloader @
      `vendor/amlogic/${DEVICE}/radio/bootloader.img`.
    
    Change-Id: I1e362296e937306f059da6e4e0c240aebc2a7921

---
+++ extract-files.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+module = ExtractUtilsModule(
+    'odroidc4',
+    'hardkernel',
+    skip_main_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, '../amlogic/sm1-common', module.vendor)
+    utils.run()
