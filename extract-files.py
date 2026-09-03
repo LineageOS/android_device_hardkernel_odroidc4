c9b4a5445dd638ec732f7936a80c977b8578dc6d
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-03 16:16:58 -0400

    odroidc4: Support including firmware on odroidc4_tab
    Change-Id: If2d03a51270598ad0abc8995ef757eb0c355e11f

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+import re
+
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -23,3 +25,15 @@ module = ExtractUtilsModule(
 if __name__ == '__main__':
     utils = ExtractUtils.device_with_common(module, '../amlogic/g12-common', module.vendor)
     utils.run()
+
+    path = f'../../../vendor/{module.vendor}/{module.device}/Android.mk'
+    with open(path) as f:
+        content = f.read()
+    content = re.sub(
+        r'ifeq \(\$\(TARGET_DEVICE\),odroidc4\)',
+        'ifneq ($(filter odroidc4 odroidc4_tab,$(TARGET_DEVICE)),)',
+        content,
+    )
+    with open(path, 'w') as f:
+        f.write(content)
+
